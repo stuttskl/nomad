98e4ceab734d9c86f09b73fa1df2d86340a1e770
Katie <25576005+stuttskl@users.noreply.github.com>
2020-07-14 21:48:39 -0700

    got the basics of react router working

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+let  app = express();
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000,  () => console.log("Example app listening on port 3000!"));
---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+  render() {
+    return (
+      <h1>hi</h1>
+    )
+  }
+}
+
+export default Home;
