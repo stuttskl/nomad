56eca846cb2ff533a1d0b7411b257522018b3eb7
Katie <25576005+stuttskl@users.noreply.github.com>
2020-07-24 22:29:19 -0700

    fixed homepage, need to fix dynamic bg color

--- src/Home.js
+++ src/Home.js
@@ -5,17 +5,19 @@ import './Home.css';
 class Home extends Component {
   render() {
     return (
-      <div className='mainContent'>
-        <div className='taglineText'>
-          dare to
-        </div>
-        <div className='homeText'>
-          redefine home
-        </div>
-        <div className='btnPosition'>
-          <button className='homeBtn'>
-            where should i go next?
-          </button>
+      <div>
+        <div className='mainContent'>
+          <div className='taglineText'>
+            dare to
+          </div>
+          <div className='homeText'>
+            redefine home
+          </div>
+          <div className='btnPosition'>
+            <button className='homeBtn'>
+              where should i go next?
+            </button>
+          </div>
         </div>
       </div>
     )
--- src/app.js
+++ src/app.js
@@ -8,39 +8,39 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  withRouter
 } from "react-router-dom";
 
-ReactDOM.render (
-    <Router>
-      <div>
-        <nav className='homeNav'>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/spotlight">Spotlight</Link>
-            </li>
-            <li>
-              <Link to="/results">Search Results</Link>
-            </li>
-          </ul>
-        </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/spotlight">
-            <Spotlight />
-          </Route>
-          <Route path="/results">
-            <SearchResultsList />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>, document.getElementById('root')
+
+ReactDOM.render (
+  <Router>
+  <div className='home'>
+    <nav className='homeNav'>
+      <ul>
+        <li>
+          <Link to="/" className='homeLink'>Home </Link>
+        </li>
+        <li>
+          <Link to="/spotlight" className='homeLink'>Spotlight </Link>
+        </li>
+        <li>
+          <Link to="/results" className='homeLink'>Search Results </Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/spotlight">
+        <Spotlight />
+      </Route>
+      <Route path="/results">
+        <SearchResultsList />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+  </div>
+  </Router>, document.getElementById('root')
   );
